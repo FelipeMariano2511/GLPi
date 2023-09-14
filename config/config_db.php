fe013ac34e5e4a494a49866bc4247ce140607c83
FelipeMariano2511 <felipe.mariano1903@gmail.com>
2023-09-14 11:39:18 -0300

    first commit

---
+++ config/config_db.php
@@ -0,0 +1,11 @@
+<?php
+class DB extends DBmysql {
+   public $dbhost = 'localhost';
+   public $dbuser = 'central_atendimento';
+   public $dbpassword = 'T3cn0R%40v%23%21';
+   public $dbdefault = 'central_atendimento_db';
+   public $use_utf8mb4 = true;
+   public $allow_myisam = false;
+   public $allow_datetime = false;
+   public $allow_signed_keys = false;
+}
